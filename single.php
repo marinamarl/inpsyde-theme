f4aeef691abd5bfc4d4b8f52209fb12e529e06cf
Marina Marlagkoutsou <marina.marlagkoutsou@gmail.com>
2018-08-19 18:17:38 +0300

    html and styles for single.php

--- single.php
+++ single.php
@@ -9,25 +9,15 @@ get_template_part( '/templates/header' );  ?>
 
   <div class="row">
     <!-- Post Content Column -->
-    <div class="content-list">
+    <div class="content-single">
 
         <?php
       if ( have_posts() ) :
         // Start the loop.
         while ( have_posts() ) : the_post();
 
-            get_template_part( 'content', get_post_format() );
-
-            // Previous/next post navigation.
-            the_post_navigation( array(
-                'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
-                    '<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
-                    '<span class="post-title">%title</span>',
-                'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
-                    '<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
-                    '<span class="post-title">%title</span>',
-            ) );
-
+            // get_template_part( 'content', get_post_format() );
+              the_content();
         // End the loop.
         endwhile; endif;
         ?>
--- templates/content.php
+++ templates/content.php
@@ -15,15 +15,8 @@
  <?php if ( has_post_thumbnail() ) { ?>
    <img class="<?php the_title_attribute(); ?>" src="<?php the_post_thumbnail_url(); ?>" alt="<?php get_post_thumbnail_id(); ?>">
    <?php
- } else {
+ } else
  echo'<p>This post has no thumbnail!</p>';
-}else :
-   the_content();
-   ?>
-   <!-- Date/Time -->
-   <div><p class="blog-info"><?php the_date(); ?> > <a href="#"><?php the_author(); ?></a> >  <?php the_category(', '); ?></p> </div>
-</div>
-<?php
  endif;
  ?>
 </div>
