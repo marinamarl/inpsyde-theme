50abc2daa7cb0ff8dad3a934a51b4d4401b54238
Marina Marlagkoutsou <marina.marlagkoutsou@gmail.com>
2018-08-22 20:39:07 +0300

    tidying up comments etc

--- home.php
+++ home.php
@@ -7,11 +7,9 @@ Tags: empty
 -->
 
 <?php get_template_part( '/templates/header' ); ?>
-<!-- it would have been get_header() but this native function only works if the specific template file is in the root directory of the theme -->
       <div class="row">
         <!-- Post Content Column -->
         <div class="content-list">
-  <!-- loop through all the posts -->
         <?php
         if ( have_posts() ) :
 
@@ -23,22 +21,14 @@ Tags: empty
             <?php
           endif;
 
-          /* Start the Loop */
           while ( have_posts() ) :
             the_post();
-            /*
-             * Include the post format-specific template for the content. If you want to
-             * use this in a child theme, then include a file called called content-___.php
-             * (where ___ is the post format) and that will be used instead.
-             */
             get_template_part( '/templates/content', get_post_format() );
 
           endwhile; endif;
           ?>
 
         </div>
-        <!-- get the sidebar template -->
-        <?php //get_template_part( '/sidebar' ); ?>
       </div>
       <!-- /.row -->
 
